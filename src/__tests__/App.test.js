4440ba01d07173cb13d29aecb72acd0bdd78673a
CarlaSlattery <cslatterydev@gmail.com>
2023-02-07 11:31:17 +0000

    first commit - set up file structure

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
+test("renders heading", () => {
+  render(<App />);
+  const headingElement = screen.getByText(/surreal estate/i);
+  expect(headingElement).toBeInTheDocument();
+});
