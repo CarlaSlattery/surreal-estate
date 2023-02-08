3c0010a5a11211baf2b613f2ac11f808d76b520d
CarlaSlattery <cslatterydev@gmail.com>
2023-02-08 15:38:11 +0000

    fixed logo and added routing to Navbar links

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../styles/App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={Properties} />
+        <Route path="/add" element={AddProperty} />
+      </Routes>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
     <div className="navBar">
-      <img
-        className="logo"
-        src="../../public/logo.png"
-        alt="surreal estate logo"
-      />
+      <img className="logo" src={logo} alt="surreal estate logo" />
       <span className="navbar-heading">Surreal Estate</span>
       <ul className="navbar-links">
-        <li className="navbar-links-item">View Properties</li>
-        <li className="navbar-links-item">Add Properties</li>
+        <li className="navbar-links-item">
+          <Link to="/">View Properties</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/add">Add Properties</Link>
+        </li>
       </ul>
     </div>
   );
